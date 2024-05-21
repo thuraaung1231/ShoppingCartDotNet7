f83798e785dcb8c3bb6f401ff9327294fec392dc
thuraaung1231 <152735945+thuraaung1231@users.noreply.github.com>
2024-05-21 15:59:00 +0630

    Dashboard

--- ShoppingCartDotNet7/Program.cs
+++ ShoppingCartDotNet7/Program.cs
@@ -1,6 +1,7 @@
 using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 using ShoppingCartDotNet7.Data;
+using MudBlazor.Services;
 
 var builder = WebApplication.CreateBuilder(args);
 
@@ -8,6 +9,7 @@ var builder = WebApplication.CreateBuilder(args);
 builder.Services.AddRazorPages();
 builder.Services.AddServerSideBlazor();
 builder.Services.AddSingleton<WeatherForecastService>();
+builder.Services.AddMudServices();
 
 var app = builder.Build();
 
